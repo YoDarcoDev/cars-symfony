a9d57313326fa6ea200ee31261fd3be016556ac4
Yoann Bettinelli <yoann.bettinelli@gmail.com>
2021-05-19 02:47:03 +0200

    js

--- public/assets/js/suppression.js
+++ public/assets/js/suppression.js
@@ -1,13 +1,28 @@
-
-
-console.log('iici');
 // SELECTION: Bouton supprimer (Croix)
-const croix = document.getElementById('croix');
+const btnCroix = document.getElementById('croix');
+console.log(btnCroix);
 
-console.log(croix)
+// SELECTION: Bouton valider (Modal)
+const btnModalOk = document.querySelector('#btnModalOk');
+console.log(btnModalOk);
 
 
-const card = document.getElementById('card');
-console.log(card)
+// FUNCTION: Remplir Modal (userId)
+const remplirModal = (userId) => {
+    btnCroix.dataset.userId = userId;
+}
 
-//
+// EVENEMENT: Click sur la croix
+btnCroix.addEventListener('click', (el) => {
+   console.log(remplirModal(el.dataset.userId));
+})
+
+// EVENEMENT: Click valider suppression
+btnModalOk.addEventListener('click', validerSuppression);
+
+
+// FUNCTION: Valider la suppression
+async function validerSuppression() {
+
+}
+
